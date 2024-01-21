7150f33786891f12ed58a9358277c9d596cbdce3
mahbub-repliq <mahbubul.alam@repliq.dev>
2024-01-21 22:50:56 +0600

    backend project setup completed

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello from express server" });
+});
+app.listen(5000, () => {
+  console.log("Server is running on localhost:5000");
+});
