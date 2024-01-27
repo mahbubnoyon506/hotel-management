4eadf6deef590bf33e9cb6677c96347a4063b61f
mahbub-repliq <mahbubul.alam@repliq.dev>
2024-01-28 02:03:21 +0600

    user creation process completed

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
+
+const userRoute = require('./routes/userRoutes')
+
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/user', userRoute)
+
+
+// connect to db
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ts2x6xa.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        // listen to port
+        app.listen(5000, () => {
+            console.log('Server requests on port 5000')
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    }) 
