06b6cbf14317156e85060d7da21eb5364528f62e
mahbub-repliq <mahbubul.alam@repliq.dev>
2024-01-29 01:54:59 +0600

    user signup api created

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
 })
 
 module.exports = userSchema;
